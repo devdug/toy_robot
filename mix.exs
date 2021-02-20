9f5b9fd23e1ce0f075950036550aaf9426169e62
devdug <gd.squarebriggs@gmail.com>
2021-02-20 13:55:52 -0800

    added exdoc dep for dev only

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule ToyRobot.MixProject do
       app: :toy_robot,
       version: "0.1.0",
       elixir: "~> 1.11",
+      escript: [main_module: ToyRobot.CLI],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
